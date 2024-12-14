da132198511a11c6a19435badc6cece47e6d997b
RAM150495 <ramaneeyansmecece2016@gmail.com>
2024-12-14 09:53:50 +0530

    update

---
+++ src/test/resources/EmployeeDetails.java
@@ -0,0 +1,21 @@
+import org.test.Employee;
+
+public class EmployeeDetails {
+	private void empID() {
+		System.out.println("My empIDis: 2046846");
+
+	}
+	private void empName() {
+		System.out.println("My Name is : Ramaneeyan");
+
+	}
+	
+	  public static void main(String[] args) {
+		  Employee e=new Employee();
+		  
+		  e.empID();
+		  e.empName();
+		  
+	}
+
+}
